1b1fe9cd9d22569c383776e320f97b2687c826dd
KangChanSong <thdrkdcks@naver.com>
2021-08-19 17:58:07 +0900

    조건부_렌더링

---
+++ hello-world/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function UserGreeting(props){
+    return <h1>Welcome Back!</h1>
+}
+
+function GuestGreeting(props){
+    return <h1>Please sign up.</h1>
+}
+
+function Greeting(props){
+    const isLoggedIn = props.isLoggedIn;
+    if(isLoggedIn){
+        return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+
+ReactDOM.render(
+    <Greeting isLoggedIn = {true} />,
+    document.getElementById('root')
+)
