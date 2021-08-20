6f865ce8f73805e8d37f1bc49deac4981c70c6a7
KangChanSong <thdrkdcks@naver.com>
2021-08-20 22:01:21 +0900

    아주_간단한_게시ㅠㅏㄴ

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,41 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Blog(props){
-
-    const sidebar = (
-        <ul>
-            {props.posts.map((post) => 
-            
-            <li key={post.id}>{post.title}</li>
-            
-            )}
-        </ul>
-    )
-
-    const content = (
-        <ul>
-            {props.posts.map((post) => 
-            <li key={post.id}>{post.content}</li>
-            )}
-        </ul>
-    )
+function Post(props){
+    const post = props.post;
+    
+    const title = (<td>{post.title}</td>)
+    const content = (<td>{post.content}</td>)
+    const writer = (<td>{post.writer}</td>)
 
     return (
-        <div>
-            {sidebar}
-            <hr />
+        <tr>
+            {title}
             {content}
-        </div>
+            {writer}
+        </tr>
+    )
+}
+
+function Board(props){
+    const posts = props.posts.map((post) => 
+                    <Post key={post.id} post={post}/>)
+
+    return (
+        <table>
+            <th>
+                <td>제목</td>
+                <td>내용</td>
+                <td>작성자</td>
+            </th>
+            {posts}
+        </table>
     )
 }
 
 const posts = [
-    {id: 1, title: '제목1' , content: '내용1'},
-    {id: 2, title: '제목2' , content: '내용2'}
+    {id:1 , title: '0213', content: '아주좋앗다.', writer:'송강찬'},
+    {id:2 , title: '오늘일기', content: '오늘은 뭘햇지.', writer:'송우리'},
+    {id:3 , title: '좋은하루', content: '재밋엇다.', writer:'송아지'}
 ]
 
 ReactDOM.render(
-    <Blog posts = {posts} />,
+    <Board posts = {posts}/>,
     document.getElementById('root')
 )
