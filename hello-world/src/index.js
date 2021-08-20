ad8e9dd50311f8e83ecb451514d766094480cc06
KangChanSong <thdrkdcks@naver.com>
2021-08-20 21:46:22 +0900

    key넣기

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function Blog(props){
 
-function NumberList(props){
-    const numbers = props.numbers;
+    const sidebar = (
+        <ul>
+            {props.posts.map((post) => 
+            
+            <li key={post.id}>{post.title}</li>
+            
+            )}
+        </ul>
+    )
 
-    const ItemList = numbers.map((n) => <li key={n.toString()}>{n}</li>)
+    const content = (
+        <ul>
+            {props.posts.map((post) => 
+            <li key={post.id}>{post.content}</li>
+            )}
+        </ul>
+    )
 
     return (
-        <ul>{ItemList}</ul>
+        <div>
+            {sidebar}
+            <hr />
+            {content}
+        </div>
     )
 }
 
-const numbers = [1,2,3,4,5];
+const posts = [
+    {id: 1, title: '제목1' , content: '내용1'},
+    {id: 2, title: '제목2' , content: '내용2'}
+]
 
 ReactDOM.render(
-    <NumberList numbers = {numbers} />,
+    <Blog posts = {posts} />,
     document.getElementById('root')
 )
